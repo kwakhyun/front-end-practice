55772ca061b267b562b26a9aa49f00856768d4ce
kwakhyun <khyun2348@naver.com>
2022-03-01 16:19:49 +0900

    switch

---
+++ switch.js
@@ -0,0 +1,32 @@
+const day = 0;
+let weekend = "";
+
+switch (day) {
+  case 1:
+    weekend = "월";
+    break;
+  case 2:
+    weekend = "화";
+    break;
+  case 3:
+    weekend = "수";
+    break;
+  case 4:
+    weekend = "목";
+    break;
+  case 5:
+    weekend = "금";
+    break;
+  case 6:
+    weekend = "토";
+    break;
+  case 7:
+    weekend = "일";
+    break;
+
+  default:
+    weekend = "error";
+    break;
+}
+
+console.log(weekend);
