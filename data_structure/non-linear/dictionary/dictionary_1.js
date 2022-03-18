039378fe390bc072b343ec71a3c55c567f5c39d4
kwakhyun <khyun2348@naver.com>
2022-03-19 00:33:53 +0900

    commit

--- data_structure/non-linear/dictionary/dictionary_1.js
+++ data_structure/non-linear/dictionary/dictionary_1.js
@@ -1,23 +1,26 @@
+// 개체(Entity)를 저장할 딕셔너리 생성자
 function Dictionary(items = {}) {
   this.items = items;
 }
 
+// 모든 개체(Entity)를 반환하는 메서드
 Dictionary.prototype.getBuffer = function () {
   return { ...this.items };
 };
 
+// 딕셔너리를 초기화하는 메서드
 Dictionary.prototype.clear = function () {
   this.items = {};
 };
-
+// 딕셔너리의 크기를 반환하는 메서드
 Dictionary.prototype.size = function () {
   return Object.keys(this.items).length;
 };
 
 let dict = new Dictionary({ age: 26, name: "kwakhyun" });
-console.log(dict);
+console.log(dict); // Dictionary { items: { age: 26, name: 'kwakhyun' } }
 
-console.log(dict.getBuffer());
-console.log(dict.size());
+console.log(dict.getBuffer()); // { age: 26, name: 'kwakhyun' }
+console.log(dict.size()); // 2
 dict.clear();
-console.log(dict);
+console.log(dict); // Dictionary { items: {} }
