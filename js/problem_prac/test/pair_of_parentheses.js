0d665fb5cf30e1b09480883878001a81019e6a29
kwakhyun <khyun2348@naver.com>
2022-04-08 00:02:28 +0900

    commit

--- js/problem_prac/test/pair_of_parentheses.js
+++ js/problem_prac/test/pair_of_parentheses.js
@@ -1,8 +1,9 @@
 // 총 4가지 종류의 괄호가 있다.
 // 괄호들은 서로 짝이 있으며 괄호가 열리고 닫히는 것을 의미한다.
-// '('')','{''}','['']','<''>'
-// 입력 : 괄호로 이루어진 문자열 S
-// 출력 : 괄호가 서로 교차하지 않는 경우 true, 교차하는 경우 false
+// '('')','{''}','['']'
+
+// input : 괄호로 이루어진 문자열 S
+// output : 괄호가 서로 교차하지 않는 경우 1, 교차하는 경우 0
 
 function solution(S) {
   const stack = [];
@@ -10,7 +11,6 @@ function solution(S) {
     "(": ")",
     "{": "}",
     "[": "]",
-    "<": ">",
   };
 
   for (let i = 0; i < S.length; i++) {
@@ -18,10 +18,13 @@ function solution(S) {
       stack.push(S[i]);
     } else {
       if (bracket[stack.pop()] !== S[i]) {
-        return false;
+        return 0;
       }
     }
   }
+
+  return stack.length === 0 ? 1 : 0;
 }
 
-console.log(solution("{[()()]}")); // true
+console.log(solution("{[()()]}")); // 1
+console.log(solution("{[(])}")); // 0
