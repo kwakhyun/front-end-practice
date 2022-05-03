0bfb15134540c14fffda95cc4e1650753d59a727
kwakhyun <khyun2348@naver.com>
2022-05-04 00:11:29 +0900

    commit

--- js/algorithm/binary_search_2.js
+++ js/algorithm/binary_search_2.js
@@ -18,6 +18,26 @@ function binarySearch_loop(arr, n) {
   return -1;
 }
 
+// 재귀 함수 기반의 이진 검색
+function binarySearch_recursive(
+  arr,
+  n,
+  lowIndex = 0,
+  highIndex = arr.length - 1
+) {
+  if (lowIndex > highIndex) return -1;
+
+  let midIndex = Math.floor((lowIndex + highIndex) / 2);
+
+  if (arr[midIndex] > n) {
+    return binarySearch_recursive(arr, n, lowIndex, midIndex - 1);
+  } else if (arr[midIndex] < n) {
+    return binarySearch_recursive(arr, n, midIndex + 1, highIndex);
+  } else {
+    return midIndex;
+  }
+}
+
 // 테스트 코드
 let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -25,3 +45,8 @@ console.log(binarySearch_loop(array, 3)); // 3
 console.log(binarySearch_loop(array, 5)); // 5
 console.log(binarySearch_loop(array, 7)); // 7
 console.log(binarySearch_loop(array, 10)); // -1
+
+console.log(binarySearch_recursive(array, 3)); // 3
+console.log(binarySearch_recursive(array, 5)); // 5
+console.log(binarySearch_recursive(array, 7)); // 7
+console.log(binarySearch_recursive(array, 10)); // -1
