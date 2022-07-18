0f2997f8e770a6c1c89f2bd150fa139cb744edeb
kwakhyun <khyun2348@naver.com>
2022-07-18 23:49:30 +0900

    commit

--- js/project/to-do-list/script.js
+++ js/project/to-do-list/script.js
@@ -11,7 +11,8 @@
   const API_URL = `http://localhost:3000/todos`;
 
   const createTodoElement = (item) => {
-    const { id, content } = item;
+    const { id, content, completed } = item;
+    const isChecked = completed ? "checked" : "";
     const $todoItem = document.createElement("div");
     $todoItem.classList.add("item");
     $todoItem.dataset.id = id;
@@ -20,6 +21,7 @@
               <input
                 type="checkbox"
                 class='todo_checkbox' 
+                ${isChecked}
               />
               <label>${content}</label>
               <input type="text" value="${content}" />
@@ -98,6 +100,74 @@
       .catch((error) => console.error(error.message));
   };
 
-  const init = () => {};
+  const changeEditMode = (e) => {
+    const $item = e.target.closest(".item");
+    const $label = $item.querySelector("label");
+    const $editInput = $item.querySelector('input[type="text"]');
+    const $contentButtons = $item.querySelector(".content_buttons");
+    const $editButtons = $item.querySelector(".edit_buttons");
+    const value = $editInput.value;
+
+    if (e.target.className === "todo_edit_button") {
+      $label.style.display = "none";
+      $editInput.style.display = "block";
+      $contentButtons.style.display = "none";
+      $editButtons.style.display = "block";
+      $editInput.focus();
+      $editInput.value = "";
+      $editInput.value = value;
+    }
+
+    if (e.target.className === "todo_edit_cancel_button") {
+      $label.style.display = "block";
+      $editInput.style.display = "none";
+      $contentButtons.style.display = "block";
+      $editButtons.style.display = "none";
+      $editInput.value = $label.innerText;
+    }
+  };
+
+  const editTodo = (e) => {
+    if (e.target.className !== "todo_edit_confirm_button") return;
+    const $item = e.target.closest(".item");
+    const id = $item.dataset.id;
+    const $editInput = $item.querySelector('input[type="text"]');
+    const content = $editInput.value;
+
+    fetch(`${API_URL}/${id}`, {
+      method: "PATCH",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ content }),
+    })
+      .then((response) => response.json())
+      .then(getTodos)
+      .catch((error) => console.error(error.message));
+  };
+
+  const removeTodo = (e) => {
+    if (e.target.className !== "todo_remove_button") return;
+    const $item = e.target.closest(".item");
+    const id = $item.dataset.id;
+
+    fetch(`${API_URL}/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(getTodos)
+      .catch((error) => console.error(error.message));
+  };
+
+  const init = () => {
+    window.addEventListener("DOMContentLoaded", () => {
+      getTodos();
+    });
+
+    $form.addEventListener("submit", addTodo);
+    $todos.addEventListener("click", toggleTodo);
+    $todos.addEventListener("click", changeEditMode);
+    $todos.addEventListener("click", editTodo);
+    $todos.addEventListener("click", removeTodo);
+  };
+
   init();
 })();
